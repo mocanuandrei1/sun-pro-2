40993a7cf9111e7596ba99cc053627201d45a85c
Ionita Alexandru <alexvladut03@yahoo.com>
2024-08-08 01:24:05 +0300

    Refactor PhotoGallery component for improved styling and responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import CTA from "@/components/CTA";
 import WhyUs from "@/components/WhyUs/WhyUs";
 import Services from "@/components/Services/Services";
 import FAQ from "@/components/FAQ/FAQ";
-import PhotoGallery from "@/components/PhotoGallery";
 
 export default function Home() {
   return (
