5cf1e9b4d9e5f4b7edfa077174dbf52748835ea2
Ionita Alexandru <alexvladut03@yahoo.com>
2024-08-15 15:20:22 +0300

    Refactor AboutStatistics component for improved layout and localization

--- src/components/AboutStatistics.js
+++ src/components/AboutStatistics.js
@@ -4,43 +4,52 @@ import CountUp from "react-countup";
 
 export default function AboutStatistics() {
   return (
-    <div className="bg-[#f68a09] text-white py-8">
-      <div className="grid lg:grid-cols-4 grid-cols-2 gap-6 lg:gap-10 mx-auto">
+    <div className="bg-[#f68a09] text-white py-8 ">
+      <div className="grid lg:grid-cols-5 grid-cols-2 gap-6 lg:gap-10 lg:mx-auto mx-10 max-w-7xl ">
         <div className="text-center">
           <CountUp
-            end={2000}
+            end={158}
             duration={3}
             suffix="+"
             className="text-4xl font-bold"
           />
-          <p className="mt-2 text-lg">Project Completed</p>
+          <p className="mt-2 text-lg">Copertine Instalate</p>
         </div>
         <div className="text-center">
           <CountUp
-            end={1500}
+            end={65}
             duration={3}
             suffix="+"
             className="text-4xl font-bold"
           />
-          <p className="mt-2 text-lg">Customer Satisfied</p>
+          <p className="mt-2 text-lg">Pergole Montate</p>
         </div>
         <div className="text-center">
           <CountUp
-            end={360}
+            end={42}
             duration={3}
             suffix="+"
             className="text-4xl font-bold"
           />
-          <p className="mt-2 text-lg">Expert Team Members</p>
+          <p className="mt-2 text-lg">Terase Închise</p>
         </div>
         <div className="text-center">
           <CountUp
-            end={85}
+            end={20}
             duration={3}
             suffix="+"
             className="text-4xl font-bold"
           />
-          <p className="mt-2 text-lg">Awards Winner</p>
+          <p className="mt-2 text-lg">Prelate PVC Instalate</p>
+        </div>
+        <div className="text-center lg:block hidden">
+          <CountUp
+            end={16}
+            duration={3}
+            suffix="+"
+            className="text-4xl font-bold"
+          />
+          <p className="mt-2 text-lg">Rulouri Exterioare Montate</p>
         </div>
       </div>
     </div>
